70f80c3eadf971a890414e4835aa3b4bd2552a3c
Jesse Taylor <114543736+jessetaylor3@users.noreply.github.com>
2023-11-02 14:31:41 -0600

    .

--- app/week7/page.js
+++ app/week7/page.js
@@ -14,23 +14,21 @@ const Page = () => {
     setItems(prevItems => [...prevItems, item]);
   };
 
-  // New event handler to handle item selection
   const handleItemSelect = (itemName) => {
-    const cleanedItemName = itemName.split(',')[0].trim().replace(/([\u2700-\u27BF]|[\uE000-\uF8FF]|�[�-�]|�[�-�]|[\u2011-\u26FF]|�[�-�])/g, '');
-    setSelectedItemName(cleanedItemName);
+    setSelectedItemName(itemName);
   };
 
   return (
     <main className="min-h-screen py-8 px-4">
       <h1 className="text-2xl font-semibold mb-6 text-center">Shopping List</h1>
       <div className="flex justify-between">
-        {/* Grouping NewItem and ItemList components */}
-        <div>
+        <div className="flex-1 mr-4">
           <NewItem onAddItem={handleAddItem} />
           <ItemList items={items} onItemSelect={handleItemSelect} />
         </div>
-        {/* MealIdeas component which takes selectedItemName as a prop */}
-        <MealIdeas ingredient={selectedItemName} />
+        <div className="flex-1">
+          <MealIdeas ingredient={selectedItemName} />
+        </div>
       </div>
     </main>
   );
